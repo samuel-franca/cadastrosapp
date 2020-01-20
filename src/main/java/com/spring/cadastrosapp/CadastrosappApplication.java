80e02754b83691f485810b7ea4c9ae5617aee29b
Samuel França <43661404+samuel-franca@users.noreply.github.com>
2020-01-20 18:29:12 -0300

    Initial commit

---
+++ src/main/java/com/spring/cadastrosapp/CadastrosappApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cadastrosapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrosappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrosappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/cadastrosapp/CadastrosappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cadastrosapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrosappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
